e161810805973dd0e737cc0676e5c49543031106
Nour Derie <nourderie41@gmail.com>
2025-08-24 20:45:10 +0300

    This my Javascript

---
+++ ch2_variable_datatypes/variables.js
@@ -0,0 +1,17 @@
+// // var name = "Nour"; // String
+// // var surname = "Derie"; // String
+// // var age = 26; // Number
+// // var isStudent = true; // Boolean
+// // console.log(name);
+// // console.log(surname);
+// // console.log(age);
+// // console.log(isStudent);
+// // console.log(age * age);
+
+// var capitalcity = null
+// // city = "London"
+// // city = "Manchester"
+// console.log(capitalcity);
+
+var theBestThingInTheWorld = 5
+console.log(typeof theBestThingInTheWorld)
